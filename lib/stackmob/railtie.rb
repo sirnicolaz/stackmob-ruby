2b74f5c0a550651af696e1e7137deb5debd25380
Jordan West <jordanrw@gmail.com>
2011-08-09 16:32:03 -0700

    [#16420705] StackMob::Railtie now loads SimpleOAuthProvider Rack middleware on init, as well as checks for existince of config/stackmob.yml file.

--- lib/stackmob/railtie.rb
+++ lib/stackmob/railtie.rb
@@ -2,10 +2,22 @@ require 'stackmob'
 require 'rails'
 
 module StackMob
-  class Railtie < Rails::Railtie
-    
+  class Railtie < Rails::Railtie        
+
     rake_tasks do
       load "stackmob/tasks.rb"
+    end
+
+    initializer "warn of missing config file" do
+      config.after_initialize do
+        unless Rails.root.join("config", "stackmob.yml").file?
+          puts "\nStackmob Config (config/stackmob.yml) not found\n"
+        end
+      end
+    end
+
+    initializer "setup middleware" do |app|
+      app.config.middleware.use StackMob::Rack::SimpleOAuthProvider
     end
 
   end
