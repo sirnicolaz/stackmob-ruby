35edc4c671fe30694d6dc7634d068e618d9969a2
tomykaira <tomykaira@gmail.com>
2011-11-28 20:48:57 +0900

    Support push to android
    - Use universal APIs
    - Handle all takens as a key-value pair containing 'type' and 'token'
    - Update unit test and integration test

--- test/unit/stackmob/test_push.rb
+++ test/unit/stackmob/test_push.rb
@@ -3,7 +3,7 @@ require 'helper'
 class StackMobPushTest < MiniTest::Unit::TestCase
 
   def setup
-    @test_token = "abcdefghijklmnopqrstuv"    
+    @test_token = { "type" => "iOS", "token" => "abcdefghijklmnopqrstuv" }
     @user_id = "123"
 
     @mock_client = mock("StackMob::Client")
@@ -15,8 +15,13 @@ class StackMobPushTest < MiniTest::Unit::TestCase
   end
 
   def test_register_call
-    @mock_client.expects(:request).with(:post, :push, "/device_tokens", :userId => @user_id, :token => @test_token).returns(nil)
+    @mock_client.expects(:request).with(:post, :push, "/register_device_token_universal", :userId => @user_id, :token => @test_token).returns(nil)
     @push.register(@user_id, @test_token)
+  end
+
+  def test_remove_call
+    @mock_client.expects(:request).with(:post, :push, "/remove_token_universal", @test_token).returns(nil)
+    @push.remove(@test_token)
   end
 
   def test_broadcast_call
@@ -24,110 +29,44 @@ class StackMobPushTest < MiniTest::Unit::TestCase
     sound = "testsound.mp3"
     alert = "testing"
 
-    expected_body = {:recipients => [], 
-                     :aps => {:alert => alert, :sound => sound, :badge => badge}, 
-                     :areRecipientsDeviceTokens => true,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push_broadcast", expected_body]
+    expected_body = { :kvPairs => {:alert => alert, :sound => sound, :badge => badge} }
+    expected_params = [:post, :push, "/push_broadcast_universal", expected_body]
     @mock_client.expects(:request).with(*expected_params).returns(nil)
 
     @push.broadcast(:badge => badge, :sound => sound, :alert => alert)
   end
 
-  def test_broadcast_missing_alert_raises_error
-    assert_raises ArgumentError do 
-      @push.broadcast(:badge => 0, :sound => "") 
-    end
-  end
-
-  def test_broadcast_badge_defaults_to_zero
-    sound = "test.mp3"
-    alert = "alert message"
-
-    expected_body = {:recipients => [], 
-                     :aps => {:alert => alert, :sound => sound, :badge => 0}, 
-                     :areRecipientsDeviceTokens => true,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push_broadcast", expected_body]
-    @mock_client.expects(:request).with(*expected_params).returns(nil)
-    @push.broadcast(:sound => sound, :alert => alert)
-  end
-
-  def test_broadcast_sound_defaults_to_empty_string
-    badge = 1
-    alert = "alert message"
-
-    expected_body = {:recipients => [], 
-                     :aps => {:alert => alert, :sound => "", :badge => badge}, 
-                     :areRecipientsDeviceTokens => true,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push_broadcast", expected_body]
-    @mock_client.expects(:request).with(*expected_params).returns(nil)
-    @push.broadcast(:badge => badge, :alert => alert)
-  end
-
-  def test_send_messsage_to_one_call
+  def test_send_messsage_to_tokens
     sound = "testsound.mp3"
     alert = "Single Message"
     badge = 2
-    token = "abc"
+    other_message = "some message"
+    android_token = { "type" => "android", "token" => "abc" }
+    ios_token = { "type" => "ios", "token" => "def" }
 
-    expected_body = {:recipients => [token],
-                     :aps => {:badge => badge, :sound => sound, :alert => alert},
-                     :areRecipientsDeviceTokens => true,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push", expected_body]
+    expected_body = {:tokens => [ios_token, android_token],
+      :payload => { :kvPairs =>
+        {:badge => badge, :audio => sound, :alert => alert,
+          :other => other_message} }}
+    expected_params = [:post, :push, "/push_tokens_universal", expected_body]
     @mock_client.expects(:request).with(*expected_params).returns(nil)
-    @push.send_message(token, :alert => alert, :sound => sound, :badge => badge)
+    @push.send_message([ios_token, android_token], :alert => alert, :audio => sound, :badge => badge, :other => other_message)
   end
 
-  def test_send_message_without_alert_raises_error
-    assert_raises ArgumentError do
-      @push.send_message("avc", :sound => "somefile.mp3", :badge => 1)
-    end
-  end
-
-  def test_send_message_defaults_badge_to_zero
-    sound = "testsound.mp3"
-    alert = "Single Message"
-    token = "abc"
-
-    expected_body = {:recipients => [token],
-                     :aps => {:badge => 0, :sound => sound, :alert => alert},
-                     :areRecipientsDeviceTokens => true,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push", expected_body]
-    @mock_client.expects(:request).with(*expected_params).returns(nil)
-    @push.send_message(token, :alert => alert, :sound => sound)
-  end
-
-  def test_send_message_defaults_sound_to_empty_string
-    alert = "Single Message"
-    badge = 2
-    token = "abc"
-
-    expected_body = {:recipients => [token],
-                     :aps => {:badge => badge, :sound => "", :alert => alert},
-                     :areRecipientsDeviceTokens => true,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push", expected_body]
-    @mock_client.expects(:request).with(*expected_params).returns(nil)
-    @push.send_message(token, :alert => alert, :badge => badge)
-  end
-
-  def test_send_message_with_user_ids
+  def test_send_messsage_with_an_user_id
     sound = "testsound.mp3"
     alert = "Single Message"
     badge = 2
-    token = "abc"
+    user = "abc"
+    other_message = "some message"
 
-    expected_body = {:recipients => [token],
-                     :aps => {:badge => badge, :sound => sound, :alert => alert},
-                     :areRecipientsDeviceTokens => false,
-                     :exclude_tokens => []}
-    expected_params = [:post, :push, "/push", expected_body]
+    expected_body = {:userIds => [user],
+      :kvPairs =>
+        {:badge => badge, :audio => sound, :alert => alert,
+          :other => other_message}}
+    expected_params = [:post, :push, "/push_users_universal", expected_body]
     @mock_client.expects(:request).with(*expected_params).returns(nil)
-    @push.send_message(token, :alert => alert, :sound => sound, :badge => badge, :recipients_are_users => true)
+    @push.send_message(user, :alert => alert, :audio => sound, :badge => badge, :other => other_message)
   end
 
 end
